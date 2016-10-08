e5990f9faa75288857ba9dc3648de8f8cad9cfce
neveron <neveron@protonmail.com>
2016-10-08 16:58:06 +0300

    start

---
+++ expression.py
@@ -0,0 +1,6 @@
+length = 5
+breadth = 2
+area = length * breadth
+
+print('площадь равна', area)
+print('периметр равен', 2 * (length + breadth))
---
+++ str_format.py
@@ -0,0 +1,12 @@
+# https://www.python.org/dev/peps/pep-3101/
+
+age = 22
+name = 'neveron'
+
+print('возраст {0} -- {1} года'.format(name, age))
+print('почему {0} забавляется с python?'.format(name))
+
+print('возраст {} -- {} года'.format(name, age))
+print('почему {} забавляется с python'.format(name))
+
+print('возраст {name} -- {age} лет'.format(name='neveron', age=22))
